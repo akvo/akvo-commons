42a1efe6f4c03e3cd858aa58e91c64ff6f3a8671
Iván Perdomo <ivan@akvo.org>
2015-04-14 18:17:22 +0200

    Initialize repo

---
+++ README.md
@@ -0,0 +1,9 @@
+# akvo-commons
+
+A Clojure utility library for Akvo platform
+
+## License
+
+Copyright © 2013-2015 Akvo Foundation
+
+Distributed under the GNU Affero General Public License 3.0
---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject org.akvo/commons "0.1.0-SNAPSHOT"
+  :description "Akvo utility library"
+  :url "http://akvo.org"
+  :license {:name "GNU Affero General Public License v3.0"
+            :url "https://www.gnu.org/licenses/agpl-3.0.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [me.raynes/fs "1.4.6"]
+                 [com.taoensso/timbre "3.3.1"]
+                 [com.google.appengine/appengine-tools-sdk "1.9.9"]
+                 [com.google.appengine/appengine-remote-api "1.9.9"]
+                 [com.google.appengine/appengine-api-1.0-sdk "1.9.9"]])
