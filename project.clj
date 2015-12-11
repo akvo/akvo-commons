a225383c5e1bf285e0d9192a7578b44f3d5391fc
Jonas Enlund <jonas.enlund@gmail.com>
2015-12-11 14:44:12 +0200

    Version 0.4.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.akvo/commons "0.4.2-SNAPSHOT"
+(defproject org.akvo/commons "0.4.2"
   :description "Akvo utility library"
   :url "http://akvo.org"
   :license {:name "GNU Affero General Public License v3.0"
