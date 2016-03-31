06398a88b906393fca6f3583446681aca13f9dd9
Daniel Karlsson <daniel@kardans.com>
2016-03-31 08:14:01 +0200

    [#22] Use = equality check

--- test/akvo/commons/psql_util_test.clj
+++ test/akvo/commons/psql_util_test.clj
@@ -11,14 +11,6 @@
    org.postgresql.util.PGobject))
 
 
-(defn- equal?
-  "No diff."
-  [v1 v2]
-  (let [[a b c] (diff v1 v2)]
-    (and (nil? a)
-         (nil? b)
-         (= v1 c))))
-
 (defn- boomerang
   "Pack and unpack via PGobject."
   [v]
@@ -29,29 +21,29 @@
 (deftest roundtrip
   (testing "nil"
 
-    (is (equal? nil
-                (boomerang nil))))
+    (is (= nil
+           (boomerang nil))))
 
   (testing "Empty sting"
-    (is (equal? ""
-                (boomerang ""))))
+    (is (= ""
+           (boomerang ""))))
 
   (testing "hash map"
-    (is (equal? {}
-                (boomerang {})))
+    (is (= {}
+           (boomerang {})))
 
-    (is (equal? {"key" "value"}
-                (boomerang {"key" "value"})))
+    (is (= {"key" "value"}
+           (boomerang {"key" "value"})))
 
-    (is (equal? {"key" {"key" "value"}}
-                (boomerang {"key" {"key" "value"}}))))
+    (is (= {"key" {"key" "value"}}
+           (boomerang {"key" {"key" "value"}}))))
 
   (testing "vector"
-    (is (equal? []
-                (boomerang [])))
+    (is (= []
+           (boomerang [])))
 
-    (is (equal? ["first" "second"]
-                (boomerang ["first" "second"]))))
+    (is (= ["first" "second"]
+           (boomerang ["first" "second"]))))
 
   ;; (testing "org.joda.time.DateTime"
   ;;   (let [t0 (t/date-time 1986 10 14 4 3 27 456)
